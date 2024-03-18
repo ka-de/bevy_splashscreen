73040f007f107f096eb27c5b5b430b5c79deb0c7
Wesley Feldsine <laocoon7@gmail.com>
2024-03-18 02:07:13 -0700

    remove iyes_progress

--- src/splash_screen_plugin.rs
+++ src/splash_screen_plugin.rs
@@ -1,6 +1,5 @@
 use bevy::prelude::*;
 use bevy_asset_loader::prelude::*;
-use iyes_progress::prelude::*;
 
 use crate::{resources::{SplashScreenConfiguration, SplashScreenImages}, state::SplashScreenState, systems::{on_enter, on_exit, update_splash_screen}};
 
@@ -14,11 +13,9 @@ impl<T: States + Clone> Plugin for SplashScreenPlugin<T> {
         // Handles SplashScreenState::Idle -> SplashScreenState::Initialize
         app.add_systems(OnEnter(self.0.run_state.clone()), on_enter::<T>);
 
-        // Handles SplashScreenState::Initialize -> SplashScreenState::Update
-        app.add_plugins(ProgressPlugin::new(SplashScreenState::Initialize).continue_to(SplashScreenState::Update));
-
         // Load all the splash screens as images
-        app.add_loading_state(LoadingState::new(SplashScreenState::Initialize).load_collection::<SplashScreenImages>());
+        // Handles SplashScreenState::Initialize -> SplashScreenState::Update
+        app.add_loading_state(LoadingState::new(SplashScreenState::Initialize).load_collection::<SplashScreenImages>().continue_to_state(SplashScreenState::Update));
 
         // Display each splash screen
         // Handles T::run_state -> T::next_state
