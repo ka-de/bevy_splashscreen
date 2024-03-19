8adb663394f28d685304a38ee38e3eb84efa8006
Wesley Feldsine <laocoon7@gmail.com>
2024-03-18 23:13:44 -0700

    add progress_tracking feature

--- src/splash_screen_plugin.rs
+++ src/splash_screen_plugin.rs
@@ -1,5 +1,7 @@
 use bevy::prelude::*;
 use bevy_asset_loader::prelude::*;
+#[cfg(feature = "progress_tracking")]
+use iyes_progress::prelude::*;
 
 use crate::{
     resources::{SplashScreenConfiguration, SplashScreenImages},
@@ -17,13 +19,29 @@ impl<T: States + Clone> Plugin for SplashScreenPlugin<T> {
         // Handles SplashScreenState::Idle -> SplashScreenState::Initialize
         app.add_systems(OnEnter(self.0.run_state.clone()), on_enter::<T>);
 
+        #[cfg(feature = "progress_tracking")]
+        {
+            app.add_plugins(
+                ProgressPlugin::new(SplashScreenState::Initialize)
+                    .continue_to(SplashScreenState::Update),
+            );
+
+            app.add_loading_state(
+                LoadingState::new(SplashScreenState::Initialize)
+                    .load_collection::<SplashScreenImages>(),
+            );
+        }
+
         // Load all the splash screens as images
         // Handles SplashScreenState::Initialize -> SplashScreenState::Update
-        app.add_loading_state(
-            LoadingState::new(SplashScreenState::Initialize)
-                .load_collection::<SplashScreenImages>()
-                .continue_to_state(SplashScreenState::Update),
-        );
+        #[cfg(not(feature = "progress_tracking"))]
+        {
+            app.add_loading_state(
+                LoadingState::new(SplashScreenState::Initialize)
+                    .load_collection::<SplashScreenImages>()
+                    .continue_to_state(SplashScreenState::Update),
+            );
+        }
 
         // Display each splash screen
         // Handles T::run_state -> T::next_state
